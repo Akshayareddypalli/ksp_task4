babc2b24abd51423a7aa90b911a73eb49ad05cf0
Akshayareddypalli <157483802+Akshayareddypalli@users.noreply.github.com>
2024-02-14 00:32:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,128 @@
+console.log("Hello world")
+/* javascript */
+
+// variables
+
+variable1=5;
+variable2=9;
+variable3=12;
+console.log(variable1);
+console.log(variable2);
+console.log(variable3);
+console.log("variable1");
+
+
+demo=12;
+demo=24;
+console.log(demo);
+console.log("demo");
+// as the previous value is deleted and updated with new value
+
+//the proper way to declare a variable is
+
+const variable4=33;
+
+console.log(variable4);
+var jeeva=33;
+console.log(jeeva);
+
+
+//Operators
+//Arithemetic Operators           + - * / % ** ++ --
+
+
+let number3=5;
+let number4=2;
+console.log(number3-number4); 
+console.log(number3 % number4); 
+
+console.log(++number3); 
+console.log(--number3); 
+console.log(number3++);
+
+let number1=2;
+console.log(number1**3);
+console.log(number1**10);
+
+
+//Assignment Operators       = += -= *= /= %= **=
+let number2=6;
+let result=number2+4;
+console.log(result);  //10
+
+let number5=6;
+number5*=2;
+console.log(number5);
+
+
+//Comparision Operator    == === != >< <= >=
+
+let number6=5;
+console.log(number6==5);
+console.log(number6==7);
+console.log(number6!=5);
+console.log(number6<7);
+console.log(number6>7);
+console.log(number6<5);
+console.log(number6>=2);
+
+//Data Types     Intergers  String  float  Boolean
+
+
+let number7=5;
+console.log(number7); // integer
+
+let number8=66.8;
+console.log(number8); // float
+
+let name="akshaya";
+console.log("name"); //string
+
+let sample=true;
+console.log(sample); // boolean
+
+
+
+// Logical Operators      && || !
+let number9=6;
+let number10=4;
+
+console.log(number9<10 && number10<5); // true
+console.log(number9<10 || number10<2); //true
+console.log(!number9<10 ||number10<2); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
